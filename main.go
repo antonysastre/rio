263fd66880576470331cdc2ad89dad8c77e598cc
Antony Sastre <antony@northcollective.se>
2026-05-24 14:02:55 +0200

    Dispatch parsed commands to handlers and write responses

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
-
-func print(s string) {
-	fmt.Println(s)
-}
 
 func main(){
 	fmt.Println("Listening on port :6379")
@@ -34,7 +31,30 @@ func main(){
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(value)
-		conn.Write([]byte("+OK\r\n"))
+
+		if value.typ != "array" {
+			fmt.Println("Invalid request, array expected")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, array > 0 expected")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		writer := NewWriter(conn)
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+		writer.Write(result)
 	}
 }
