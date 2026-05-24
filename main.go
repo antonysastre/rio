e822c19cbd4c68ba7d764a46aff5706a78c95223
Antony Sastre <antony@northcollective.se>
2026-05-24 14:41:32 +0200

    Added append to file persistence (AOF)

--- main.go
+++ main.go
@@ -15,15 +15,31 @@ func main(){
 		return
 	}
 
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+		handler(args)
+	})
+
 	conn, err := srvr.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	defer conn.Close()
 
-	// input := "$5\r\nMando\r\n"
 	for {
 		resp := NewResp(conn)
 		value, err := resp.Read()
@@ -52,6 +68,10 @@ func main(){
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)
